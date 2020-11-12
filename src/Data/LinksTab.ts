dff6417607af84ceb697c2891a9a72c57780ea1c
Kaue Recio Carpim Sala <56834668+reciokaue@users.noreply.github.com>
2020-11-12 17:43:55 -0300

    Add files via upload

---
+++ src/Data/LinksTab.ts
@@ -0,0 +1,492 @@
+export default[
+    {
+        title: "homem",
+        columns: [
+            {
+                subtitle: "Tenis", drop: true,
+                itens: [
+                    { name: "TODOS OS TÊNIS"},
+                    { name: "NOVIDADES"},
+                    { name: "SUPERSTAR"},
+                    { name: "ORIGINALS"},
+                    { name: "FUTEBOL"},
+                    { name: "RUNNING"},
+                    { name: "PREÇO IRRESISTÍVEL"},
+                    { name: "TERREX"},
+                    { name: "SKATEBOARDING"},
+                    { name: "ESSENCIAIS"},
+                    { name: "TENNIS"},
+                    { name: "BASQUETE"},
+                    { name: "CHINELOS E SANDÁLIAS"},
+                    { name: "OUTLET"},
+                ]
+            },
+            { 
+                subtitle: "ROUPAS" , drop: true,
+                itens: [
+                    { name: "TODAS AS ROUPAS"},
+                    { name: "NOVIDADES"},
+                    { name: "CAMISETAS"},
+                    { name: "POLO"},
+                    { name: "SHORTS E BERMUDAS"},
+                    { name: "MOLETONS E CASACOS"},
+                    { name: "CAMISAS DE FUTEBOL"},
+                    { name: "JAQUETAS"},
+                    { name: "AGASALHOS"},
+                    { name: "UNISSEX"},
+                    { name: "CALÇAS E LEGGINGS"},
+                    { name: "FARDAMENTO"},
+                    { name: "SEGUNDA PELE"},
+                    { name: "NATAÇÃO"},
+                    { name: "TERREX"},
+                    { name: "SUNGAS"},
+                    { name: "OUTLET"},
+                ]
+            },
+            { 
+                subtitle: "ACESSÓRIOS" , drop: true,
+                itens: [
+                    { name: "TODOS OS ACESSÓRIOS"},
+                    { name: "NOVIDADES"},
+                    { name: "BOLSAS E MOCHILAS"},
+                    { name: "MEIAS"},
+                    { name: "MÁSCARA"},
+                    { name: "BONÉS"},
+                    { name: "BOLAS"},
+                    { name: "CANELEIRAS"},
+                    { name: "LUVAS"},
+                    { name: "SQUEEZES E GARRAFAS"},
+                    { name: "OUTLET"},
+                ]
+            },
+            { 
+                subtitle: "LANÇAMENTOS" , drop: true,
+                itens: [
+                    { name: "TÊNIS"},
+                    { name: "ROUPAS"},
+                    { name: "ACESSÓRIOS"},
+                ]
+            },
+            { 
+                subtitle: "TENDÊNCIAS" , drop: true,
+                itens: [
+                    { name: "SLIP ON"},
+                    { name: "UNISSEX"},
+                    { name: "BAYERN MUNICH"},
+                    { name: "BOOST SINTA A ENERGIA"},
+                    { name: "TÊNIS BRANCO"},
+                    { name: "ROUPAS CONFORTAVEIS"},
+                    { name: "TREINO EM CASA"},
+                    { name: "O ÚLTIMO EM CHUTEIRAS"},
+                    { name: "MAIS VENDIDOS"},
+                ]
+            },
+            { subtitle: "CRUZEIRO", drop: false, itens: [], },
+            { subtitle: "INTERNACIONAL", drop: false, itens: [], },
+            { subtitle: "SÃO PAULO", drop: false, itens: [], },
+            { subtitle: "FLAMENGO", drop: false, itens: [], },
+            {
+                subtitle: "PRODUTOS RECICLADOS", drop: true,
+                itens: [
+                    { name: "PLÁSTICO NOS OCEANOS"},
+                    { name: "POLIÉSTER RECICLADO"},
+                    { name: "TODOS OS PRODUTOS RECICLADOS"},
+                ]
+            },
+            { subtitle: "OUTLET", drop: false, itens: [], },
+            { subtitle: "PRESENTES PARA HOMEM NOVO!", drop: false, itens: [], },
+            { subtitle: "COLEÇÃO VERÃO NOVO!", drop: false, itens: [], },
+            { subtitle: "MÁSCARA", drop: false, itens: [], },
+            { subtitle: "EXCLUSIVO ONLINE", drop: false, itens: [], },
+            { subtitle: "ADIDAS APP", drop: false, itens: [], },
+            { subtitle: "VER TODOS", drop: false, itens: [], },
+            { subtitle: "PERSONALIZAÇÃO", drop: false, itens: [], },
+            { subtitle: "PRÓXIMOS LANÇAMENTOS", drop: false, itens: [], },
+        ],
+    },
+    {
+        title: "mulher",
+        columns: [
+            {
+                subtitle: "TÊNIS", drop: true,
+                itens: [
+                    { name: "TODOS OS TÊNIS"},
+                    { name: "NOVIDADES"},
+                    { name: "SUPERSTAR"},
+                    { name: "ORIGINALS"},
+                    { name: "SLIP ON"},
+                    { name: "RUNNING"},
+                    { name: "TRAINING"},
+                    { name: "PREÇO IRRESISTÍVEL"},
+                    { name: "TERREX"},
+                    { name: "ESSENCIAIS"},
+                    { name: "CHUTEIRAS"},
+                    { name: "CHINELOS E SANDÁLIAS"},
+                    { name: "OUTLET"},
+                ]    
+            },
+            {
+                subtitle: "ROUPAS", drop: true,
+                itens: [
+                    { name: "TODAS AS ROUPAS"},
+                    { name: "NOVIDADES"},
+                    { name: "CAMISETAS"},
+                    { name: "TOPS ESPORTIVOS"},
+                    { name: "SHORTS"},
+                    { name: "VESTIDOS"},
+                    { name: "MOLETONS E CASACOS"},
+                    { name: "TAMANHO GG - 4GG"},
+                    { name: "YOGA"},
+                    { name: "UNISSEX"},
+                    { name: "JAQUETAS"},
+                    { name: "AGASALHOS"},
+                    { name: "CALÇAS E LEGGINGS"},
+                    { name: "SAIAS"},
+                    { name: "TERREX"},
+                    { name: "MAIÔS"},
+                    { name: "OUTLET"},
+                ]
+            },
+            {
+                subtitle: "ACESSÓRIOS", drop: true,
+                itens: [
+                    { name: "TODOS OS ACESSÓRIOS"},
+                    { name: "BOLSAS E MOCHILAS"},
+                    { name: "MEIAS"},
+                    { name: "MÁSCARA"},
+                    { name: "BONÉS"},
+                    { name: "BOLAS"},
+                    { name: "SQUEEZES E GARRAFAS"},
+                    { name: "OUTLET"},
+                ]
+            },
+            {
+                subtitle: "LANÇAMENTOS", drop: true,
+                itens: [
+                    { name: "TÊNIS"},
+                    { name: "ROUPAS"},
+                    { name: "ACESSÓRIOS"},
+                    { name: "TUDO PARA MULHERES"},
+                ]
+            },
+            {
+                subtitle: "TENDÊNCIAS", drop: true,
+                itens: [
+                    { name:"UNISSEX"},
+                    { name:"ZX"},
+                    { name:"BOOST SINTA A ENERGIA"},
+                    { name:"MODA SUSTENTÁVEL"},
+                    { name:"TÊNIS BRANCO"},
+                    { name:"HEAT RDY"},
+                    { name:"ROUPAS CONFORTAVEIS"},
+                    { name:"SUPERSTAR"},
+                    { name:"MAIS VENDIDOS"},
+                    { name:"STAN SMITH"},
+                    { name:"ROUPAS DE ACADEMIA"},
+                ]
+            },
+            { subtitle: "FLAMENGO", drop: false, itens: []},
+            { subtitle: "CRUZEIRO", drop: false, itens: []},
+            { subtitle: "INTERNACIONAL", drop: false, itens: []},
+            { subtitle: "SÃO PAULO", drop: false, itens: []},
+            { subtitle: "PRODUTOS RECICLADOS", drop: false, itens: []},
+            { subtitle: "PLÁSTICO NOS OCEANOS", drop: false, itens: []},
+            { subtitle: "POLIÉSTER RECICLADO", drop: false, itens: []},
+            {
+                subtitle: "PRODUTOS RECICLADOS", drop: true,
+                itens: [
+                    { name: "PLÁSTICO NOS OCEANOS"},
+                    { name: "POLIÉSTER RECICLADO"},
+                    { name: "TODOS OS PRODUTOS RECICLADOS"},
+                ]
+            },
+            { subtitle: "OUTLET", drop: false, itens: []},
+            { subtitle: "PRESENTES PARA MULHER NOVO!", drop: false, itens: []},
+            { subtitle: "SLIP ON", drop: false, itens: []},
+            { subtitle: "MÁSCARA", drop: false, itens: []},
+            { subtitle: "COLEÇÃO VERÃO NOVO!", drop: false, itens: []},
+            { subtitle: "EXCLUSIVO ONLINE", drop: false, itens: []},
+            { subtitle: "VER TODOS", drop: false, itens: []},
+            { subtitle: "PERSONALIZAÇÃO", drop: false, itens: []},
+            { subtitle: "PRÓXIMOS LANÇAMENTOS", drop: false, itens: []},
+        ],
+    },
+    {
+        title: "kids",
+        columns: [
+            {
+                subtitle: "JUVENIL (8-16)", drop: true,
+                itens: [
+                    { name: "VER TODOS OS TÊNIS"},
+                    { name: "TÊNIS MENINAS"},
+                    { name: "TÊNIS MENINOS"},
+                    { name: "TÊNIS ORIGINALS"},
+                    { name: "TÊNIS ESPORTES"},
+                    { name: "OUTLET TÊNIS"},
+                    { name: "VER TODAS AS ROUPAS"},
+                    { name: "ROUPAS MENINOS"},
+                    { name: "ROUPAS MENINAS"},
+                    { name: "CAMISETAS"},
+                    { name: "OUTLET ROUPAS"},
+                    { name: "OUTLET"},
+                    { name: "VER TODOS"},
+                ]
+            },{
+                subtitle: "KIDS (4-8)", drop: true,
+                itens: [
+                    { name: "VER TODOS OS TÊNIS"},
+                    { name: "TÊNIS MENINAS"},
+                    { name: "TÊNIS MENINOS"},
+                    { name: "TÊNIS ORIGINALS"},
+                    { name: "CHUTEIRAS"},
+                    { name: "TÊNIS ESPORTES"},
+                    { name: "OUTLET TENIS"},
+                    { name: "VER TODAS AS ROUPAS"},
+                    { name: "ROUPAS MENINOS"},
+                    { name: "ROUPAS MENINAS"},
+                    { name: "CAMISETAS"},
+                    { name: "OUTLET ROUPAS"},
+                    { name: "VER TODOS"},
+                ]
+            },{
+                subtitle: "BEBÊ (0-4)", drop: true,
+                itens: [
+                    { name: "VER TODOS OS TÊNIS"},
+                    { name: "TÊNIS MENINAS"},
+                    { name: "TÊNIS MENINOS"},
+                    { name: "TÊNIS ORIGINALS"},
+                    { name: "TÊNIS ESPORTES"},
+                    { name: "OUTLET TENIS"},
+                    { name: "VER TODAS AS ROUPAS"},
+                    { name: "ROUPAS MENINAS"},
+                    { name: "ROUPAS MENINOS"},
+                    { name: "CAMISETAS"},
+                    { name: "AGASALHOS"},
+                    { name: "OUTLET ROUPAS"},
+                    { name: "VER TODOS"},
+                ]
+            },
+            { subtitle: "SÃO PAULO", drop: false, itens: []},
+            { subtitle: "CRUZEIRO", drop: false, itens: []},
+            { subtitle: "FLAMENGO", drop: false, itens: []},
+            {
+                subtitle: "LANÇAMENTOS", drop: true,
+                itens: [
+                    { name: "TÊNIS"},
+                    { name: "ROUPAS"},
+                    { name: "ACESSÓRIOS"},
+                    { name: "TUDO PARA KIDS"},
+                ]
+            },{
+                subtitle: "TENDÊNCIAS", drop: true,
+                itens: [
+                    { name: "MAIS VENDIDOS"},
+                    { name: "PRETO E BRANCO"},
+                    { name: "TOY STORY"},
+                    { name: "TERREX"},
+                    { name: "O ÚLTIMO EM CHUTEIRAS"},
+                    { name: "ADICOLOR"},
+                ]
+            },
+            { subtitle: "OUTLET", drop: false, itens: []},
+            { subtitle: "PRESENTES PARA KIDS NOVO!", drop: false, itens: []},
+            { subtitle: "COLEÇÃO VERÃO NOVO!", drop: false, itens: []},
+            { subtitle: "RECÉM NASCIDO", drop: false, itens: []},
+            { subtitle: "MÁSCARA", drop: false, itens: []},
+            { subtitle: "DISNEY", drop: false, itens: []},
+            { subtitle: "VER TODOS", drop: false, itens: []},
+            { subtitle: "PERSONALIZAÇÃO", drop: false, itens: []},
+        ],
+    },
+    {
+        title: "PRESENTES",
+        columns: [
+            {
+                subtitle: "PARA HOMEM", drop: true,
+                itens: [
+                    {name: "IDEIAS DE PRESENTES"},
+                    {name: "VER TODOS OS TÊNIS"},
+                    {name: "VER TODAS AS ROUPAS"},
+                    {name: "PRESENTES SUSTENTÁVEIS"},
+                    {name: "CAMISETAS ESTAMPADAS"},
+                    {name: "ROUPAS CONFORTAVEIS"},
+                    {name: "COLEÇÃO PRETO E BRANCO"},
+                    {name: "PRESENTES LIFESTYLE"},
+                    {name: "PRESENTES UNISSEX"},
+                    {name: "VER TODOS OS ACESSÓRIOS"},
+                    {name: "MEIAS                    "},
+                ]
+            },{
+                subtitle: "PARA MULHER", drop: true,
+                itens: [
+                    { name: "IDEIAS DE PRESENTES"},
+                    { name: "VER TODOS OS TÊNIS"},
+                    { name: "VER TODAS AS ROUPAS"},
+                    { name: "PRESENTES SUSTENTÁVEIS"},
+                    { name: "CAMISETAS ESTAMPADAS"},
+                    { name: "ROUPAS CONFORTAVEIS"},
+                    { name: "COLEÇÃO PRETO E BRANCO"},
+                    { name: "PRESENTES LIFESTYLE"},
+                    { name: "PRESENTES UNISSEX"},
+                    { name: "VER TODOS OS ACESSÓRIOS"},
+                    { name: "MEIAS"},
+                ]
+            },{
+                subtitle: "PARA KIDS", drop: false,
+                itens: [
+                    { name: "IDEIAS DE PRESENTES"},
+                    { name: "VER TODOS OS TÊNIS"},
+                    { name: "VER TODAS AS ROUPAS"},
+                    { name: "RECÉM NASCIDO"},
+                    { name: "DISNEY"},
+                    { name: "CAMISETAS ESTAMPADAS"},
+                    { name: "ROUPAS CONFORTAVEIS"},
+                    { name: "COLEÇÃO PRETO E BRANCO"},
+                    { name: "PRESENTES LIFESTYLE"},
+                    { name: "VER TODOS OS ACESSÓRIOS"},
+                    { name: "MEIAS"},
+                ]
+            },{
+                subtitle: "PRESENTES POR PREÇO", drop: true,
+                itens: [
+                    { name: "PRESENTES POR MENOS DE R$300"},
+                    { name: "PRESENTES POR MENOS DE R$400"},
+                    { name: "PRESENTES POR MENOS DE R$550"},
+                ]
+            },{
+                subtitle: "FANÁTICOS POR FUTEBOL", drop: true,
+                itens: [
+                    { name: "FLAMENGO"},
+                    { name: "SÃO PAULO"},
+                    { name: "INTERNACIONAL"},
+                    { name: "CRUZEIRO"},
+                    { name: "REAL MADRID"},
+                    { name: "JUVENTUS"},
+                    { name: "MANCHESTER UNITED"},
+                    { name: "BAYERN DE MÚNICH"},
+                    { name: "ARSENAL"},
+                ]
+            }
+        ]
+    },
+    {
+        title: "MARCAS",
+        columns: [
+            {
+                subtitle: "ORIGINALS", drop: true,
+                itens: [
+                    {name: "TÊNIS"},
+                    {name: "ROUPAS"},
+                    {name: "ACESSÓRIOS"},
+                    {name: "ZX"},
+                    {name: "NITE JOGGER"},
+                    {name: "NMD"},
+                    {name: "STAN SMITH"},
+                    {name: "SUPERSTAR"},
+                    {name: "ADICOLOR"},
+                    {name: "TODOS ORIGINALS"},
+                ]
+            },{
+                subtitle: "ATHLETICS", drop: true,
+                itens: [
+                    { name: "ROUPAS PARA HOMEM"},
+                    { name: "ROUPAS PARA MULHER"},
+                    { name: "ESSENTIALS"},
+                    { name: "ZNE"},
+                    { name: "PRODUCTOS ATHLETICS"},
+                ]
+            },{
+                subtitle: "STELLA MCCARTNEY", drop: true,
+                itens: [
+                    { name: "TÊNIS"},
+                    { name: "ROUPAS"},
+                    { name: "TODOS STELLA MCCARTNEY"},
+                ]
+            },{
+                subtitle: "TERREX", drop: true,
+                itens: [
+                    { name: "TÊNIS"},
+                    { name: "ROUPAS"},
+                    { name: "TODOS TERREX"},
+                ]
+            },
+            {
+                subtitle: "PRODUTOS RECICLADOS", drop: true,
+                itens: [
+                    { name: "PLÁSTICO NOS OCEANOS"},
+                    { name: "POLIÉSTER RECICLADO"},
+                    { name: "TODOS OS PRODUTOS RECICLADOS"},
+                ]
+            },
+        ]
+    },
+    {
+        title: "ESPORTES",
+        columns: [
+            {
+                subtitle: "FUTEBOL", drop: true,
+                itens: [
+                    { name: "CHUTEIRAS"},
+                    { name: "ROUPAS"},
+                    { name: "ACESSÓRIOS"},
+                    { name: "CLUBES DE FUTEBOL"},
+                    { name: "FLAMENGO"},
+                    { name: "SÃO PAULO"},
+                    { name: "REAL MADRID"},
+                    { name: "JUVENTUS"},
+                    { name: "TODOS FUTEBOL"},
+                ]
+            },{
+                subtitle: "CORRIDA", drop: true,
+                itens: [
+                    { name: "TÊNIS"},
+                    { name: "ROUPAS"},
+                    { name: "ACESSÓRIOS"},
+                    { name: "RUN FOR THE OCEANS"},
+                    { name: "ULTRABOOST"},
+                    { name: "ADIZERO"},
+                    { name: "PULSEBOOST"},
+                    { name: "TODOS RUNNING   "},
+                ]
+            },{
+                subtitle: "ACADEMIA E TREINO", drop: true,
+                itens: [
+                    { name: "TÊNIS"},
+                    { name: "ROUPAS"},
+                    { name: "BOLSAS ESPORTES"},
+                    { name: "RDY"},
+                    { name: "ALPHASKIN"},
+                    { name: "PRIMEKNIT"},
+                    { name: "TODOS TRAINING"},
+                ]
+            },{
+                subtitle: "BASQUETE", drop: true,
+                itens: [
+                    { name: "TÊNIS"},
+                    { name: "ROUPAS"},
+                    { name: "ACESSÓRIOS"},
+                    { name: "TODOS BASQUETE"},
+                ]
+            },{
+                subtitle: "SKATEBOARDING", drop: true,
+                itens: [
+                    { name: "TÊNIS"},
+                    { name: "ROUPAS"},
+                    { name: "ACESSÓRIOS"},
+                    { name: "3MC"},
+                    { name: "TODOS SKATEBOARDING"},
+                ]
+            },{
+                subtitle: "OUTROS ESPORTES", drop: true,
+                itens: [
+                    { name: "AVENTURA"},
+                    { name: "TENNIS"},
+                    { name: "NATAÇÃO"},
+                    { name: "YOGA"},
+                    { name: "HANDBALL"},
+                    { name: "VOLEI"},
+                ]
+            }
+        ]
+    }
+]
